e26cb64adc79404944659f25025aa7b387d26867
Scott Rutz <50925884+rutzsco@users.noreply.github.com>
2021-05-19 13:00:00 -0500

    Update Program.cs

--- Demo.API/Program.cs
+++ Demo.API/Program.cs
@@ -14,7 +14,7 @@ namespace DemoAPI
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            CreateHostBuilder(args).Build().Run()
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
