742748e03758def4e8e942a5b3d88296ee6cfb93
Scott Rutz <scrutz@microsoft.com>
2021-05-19 12:05:58 -0500

    cp

---
+++ Demo.API/Controllers/StatusController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoAPI.Controllers
+{
+    [ApiController]
+    public class StatusController : Controller
+    {
+        [HttpGet("status")]
+        public IActionResult Get()
+        {
+            var version = "Unknown";
+            var variables = Environment.GetEnvironmentVariables();
+            if (variables.Contains("APPLICATION_VERSION"))
+                version = variables["APPLICATION_VERSION"].ToString();
+
+            var vm = new { Version = version };
+            return new OkObjectResult(vm);
+        }
+
+        [HttpGet("status/full")]
+        public IEnumerable<KeyValuePair<string, string>> GetFull()
+        {
+            var variables = new List<KeyValuePair<string, string>>();
+            foreach (DictionaryEntry de in Environment.GetEnvironmentVariables())
+                variables.Add(new KeyValuePair<string, string>(de.Key.ToString(), de.Value.ToString()));
+            return variables;
+        }
+    }
+}
